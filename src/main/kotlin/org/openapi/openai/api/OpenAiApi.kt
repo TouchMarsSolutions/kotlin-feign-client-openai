9df86ddc4cfcda127379a21606db07167e63a569
John <jhcao23@gmail.com>
2023-02-15 23:26:25 -0800

    refac

--- src/main/kotlin/org/openapi/openai/api/OpenAiApi.kt
+++ src/main/kotlin/org/openapi/openai/api/OpenAiApi.kt
@@ -3,7 +3,7 @@
  * https://openapi-generator.tech
  * Do not edit the class manually.
  */
-package org.openai.api
+package org.openapi.openai.api
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
